f464b3663e3c72b5b20b967d1d09677624f33f0e
Niru <neiladrian.villegas@gmail.com>
2019-11-14 01:57:52 +0800

    models

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart';
+import 'models/location.dart';
 
 class LocationDetail extends StatelessWidget {
+  final Location location;
+
+  LocationDetail(this.location);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("Sample Titles"),
+        title: Text(this.location.name),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          _section("One", Colors.red),
-          _section("Two", Colors.green),
-          _section("Three", Colors.yellow),
-        ],
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // _section("One", Colors.red),
+            // _section("Two", Colors.green),
+            // _section("Three", Colors.yellow),
+            ...render(this.location),
+          ],
+        ),
       ),
     );
+  }
+
+  List<Widget> render(Location location) {
+    var result = List<Widget>();
+    for (var fact in location.facts) {
+      result.add(_sectionTitle(fact.title));
+      result.add(_sectionText(fact.text));
+    }
+    return result;
+  }
+
+  Widget _sectionTitle(String text) {
+    return Container(
+      margin: EdgeInsets.only(top:8, bottom: 8),
+      child: Text(
+        text,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+      ),
+    );
+  }
+
+  Widget _sectionText(String text) {
+    return Text(text);
   }
 
   Widget _section(String title, Color color) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import './location_detail.dart';
+import 'mocks/mock_location.dart';
+import 'models/location.dart';
 
 void main() {
+  final Location mockLocation = MockLocation.FetchAny();
   return runApp(
     MaterialApp(
-      home: LocationDetail(),
+      home: LocationDetail(mockLocation),
     ),
   );
 }
