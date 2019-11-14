749b7deb4dbcf3d6345800caae6cb814ec20c327
Niru <neiladrian.villegas@gmail.com>
2019-11-15 01:30:13 +0800

    update UI styles

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -12,56 +12,95 @@ class LocationList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(
-            "Locations",
-            style: Styles.navBarTitleStyle,
-          ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          "Locations",
+          style: Styles.navBarTitleStyle,
         ),
-        body: ListView(
-          children: locations
-              .map((location) => _listItemBuilder(context, location))
-              .toList(),
-        )
-        // ListView.builder(
-        //   itemCount: this.locations.length,
-        //   itemBuilder: (context, index) {
-        //     return _listItemBuilder(context, this.locations[index]);
-        //   },
-        // ),
-        );
+      ),
+      body:
+          //not good because implements like listview in android
+          //generate everything
+          // ListView(
+          //   children: locations
+          //       .map((location) => _listItemBuilder(context, location))
+          //       .toList(),
+          // ),
+          //implements like recyclerview in android
+          //generate only the visible part of the screen
+          ListView.builder(
+        itemCount: this.locations.length,
+        itemBuilder: (context, index) {
+          return _listItemBuilder(context, this.locations[index]);
+        },
+      ),
+    );
   }
 
   Widget _listItemBuilder(BuildContext context, Location location) {
     return Container(
-        height: 100,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: NetworkImage(location.url), fit: BoxFit.cover),
-        ),
-        child: Container(
-          // color: Colors.red,
-          height: 16,
-          child: ListTile(
-            contentPadding: EdgeInsets.all(10.0),
-            subtitle: Text(
-              location.facts[0].text,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                color: Colors.white,
-              ),
+      height: 200.0,
+      child: Stack(
+        children: [
+          Container(
+            constraints: BoxConstraints.expand(height: 200.0),
+            decoration: BoxDecoration(
+              color: Colors.grey,
             ),
-            // leading: _itemThumbnail(location),
-            title: GestureDetector(
-                child: _itemTitle(location),
-                onTap: () => _navigateToLocationDetail(context, location)),
-            // onTap: () {
-            //   _navigateToLocationDetail(context, location);
-            // },
+            child: Container(
+              constraints: BoxConstraints.tightFor(width: 200),
+              child: Image.network(location.url, fit: BoxFit.cover),
+            ),
           ),
-        ));
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 12.0,vertical:8.0),
+                  decoration:
+                      BoxDecoration(color: Colors.black.withOpacity(0.5)),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                          child: _itemTitle(location),
+                          onTap: () =>
+                              _navigateToLocationDetail(context, location)),
+                      Text(
+                        location.facts[0].text,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  )),
+            ],
+          ),
+          // ListTile(
+          //   contentPadding: EdgeInsets.all(10.0),
+          //   subtitle: Text(
+          //     location.facts[0].text,
+          //     maxLines: 2,
+          //     overflow: TextOverflow.ellipsis,
+          //     style: TextStyle(
+          //       color: Colors.white,
+          //     ),
+          //   ),
+          //   // leading: _itemThumbnail(location),
+          //   title: GestureDetector(
+          //       child: _itemTitle(location),
+          //       onTap: () => _navigateToLocationDetail(context, location)),
+          //   // onTap: () {
+          //   //   _navigateToLocationDetail(context, location);
+          //   // },
+          // ),
+          // )
+        ],
+      ),
+    );
   }
 
   void _navigateToLocationDetail(context, location) {
@@ -82,12 +121,15 @@ class LocationList extends StatelessWidget {
   }
 
   Widget _itemTitle(Location location) {
-    return Text(
-      '"${location.name}"',
-      style: TextStyle(
-        fontSize: 16,
-        color: Colors.white,
-        fontWeight: FontWeight.bold
+    return Container(
+      width: double.infinity,
+      child: Text(
+        '"${location.name}"',
+        textAlign: TextAlign.left,
+        overflow: TextOverflow.ellipsis,
+        maxLines: 1,
+        style: TextStyle(
+            fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
       ),
     );
   }
